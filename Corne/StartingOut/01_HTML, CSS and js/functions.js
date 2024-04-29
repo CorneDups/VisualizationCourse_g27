a8add4404ce041085cf69ca9a2d050345cfb1c88
Corné du Plooy <corne.duplooy@kuleuven.be>
2024-04-29 17:48:24 +0200

    Corne 29 april update

---
+++ Corne/StartingOut/01_HTML, CSS and js/functions.js
@@ -0,0 +1,12 @@
+// First version
+function function_1(a,b) { return a + b }
+
+// Second version
+let function_2 = function(a,b) { return a + b }
+
+// Third version
+let function_3 = (a,b) => { return a + b }
+
+console.log(function_1(1,2))
+console.log(function_2(1,2))
+console.log(function_3(1,2))
